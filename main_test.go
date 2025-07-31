75ee5c608df880d2c8e3e4063ec2a6a9a7bf1267
Hergoln <juliusz.malka@o2.pl>
2025-07-31 21:33:51 +0200

    added testdata

--- main_test.go
+++ main_test.go
@@ -1,12 +1,26 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+var (
+	TEST_RESOURCES_PATH = "testdata"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func TestMain(t *testing.T) {
 	t.Run("main test case", func(t *testing.T) {
-		got := "random string"
-		if got != "random string" {
-			t.Error("This dummy test should never fail.")
-		}
+		data, err := os.ReadFile(TEST_RESOURCES_PATH + "/Better_Motherfucking_Website.html")
+		check(err)
+
+		fmt.Println(string(data))
 	})
 }
