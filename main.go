8ce8c5aaaa15a392a3f2c6acf2eb560310e4205f
Hergoln <juliusz.malka@o2.pl>
2025-08-03 20:03:43 +0200

    added initial implementation of gatherHRefs function

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
+var (
+	path *string
+)
+
+func parseInput() {
+	path = flag.String("path", "", "path to html file to read")
+	flag.Parse()
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	parseInput()
+	fmt.Println(*path)
+	data, err := os.ReadFile(*path)
+	check(err)
+
+	hrefs := gatherHRefs(data)
+
+	fmt.Println(hrefs)
+}
+
+func gatherHRefs(data []byte) []string {
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -10,17 +10,10 @@ var (
 	TEST_RESOURCES_PATH = "testdata"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 func TestMain(t *testing.T) {
 	t.Run("main test case", func(t *testing.T) {
-		data, err := os.ReadFile(TEST_RESOURCES_PATH + "/Better_Motherfucking_Website.html")
-		check(err)
+		data, _ := os.ReadFile(TEST_RESOURCES_PATH + "/Better_Motherfucking_Website.html")
 
-		fmt.Println(string(data))
+		fmt.Println(data)
 	})
 }
